a480d18b1266e181585a202e7d06cc1b7099cd59
Dany Paredes <dany.paredes@external.wolterskluwer.com>
2023-04-16 16:56:48 +0200

    testing functional guard

--- src/app/guards/domain.guard.spec.ts
+++ src/app/guards/domain.guard.spec.ts
@@ -1,36 +1,51 @@
 import { TestBed } from "@angular/core/testing";
 import {RouterTestingModule} from "@angular/router/testing";
-import * as domain from "domain";
+
 import {domainGuard} from "./domain.guard";
 import {DomainService} from "../domain.service";
-import {HttpTestingController} from "@angular/common/http/testing";
-import {DomainsGuard} from "../domains.guard";
+
+import {routes} from "../routes";
+import {lastValueFrom, of} from "rxjs";
+import {Router} from "@angular/router";
 
 describe('Domain Guard', () => {
 
-  beforeEach((): void => {
-    // const serviceUrlSpy: jasmine.Spy<() => ServiceUrl> = spyOn(
-    //   ServiceUrl,
-    //   "getInstance"
-    // ).and.returnValue({
-    //   setUrlData: (url: string): void => {},
-    //   get: (keyName: string): any => ({ UrlRead: fakeTfsApiUrl }),
-    //   getAll: (): object => null,
-    //   urlData: {}
-    // });
-    //
-    const domainServiceSpy =
-       jasmine.createSpyObj<DomainService>(["isAvailable"]);
+  const mockRouter = jasmine.createSpyObj<Router>(['navigate'])
+  mockRouter.navigate.and.returnValue(lastValueFrom(of(true)))
 
+  const setup = (domainServiceMock: unknown) => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
       providers: [
         domainGuard,
-        { provide: DomainService, useValue: domainServiceSpy }
+        { provide: DomainService, useValue: domainServiceMock},
+        { provide: Router, useValue: mockRouter}
       ]
     });
 
-    const guard = TestBed.runInInjectionContext(domainGuard);
-    const httpMock = TestBed.inject(HttpTestingController);
-  });
+    return TestBed.runInInjectionContext(domainGuard);
+  }
+
+  it('should allow to continue', () => {
+
+    const mockDomainService : unknown = {
+      isAvailable: () => of(true)
+    }
+    const guard = setup(mockDomainService);
+    guard.subscribe((p: unknown) => {
+      expect(p).toBe(true)
+    })
+  })
+
+  it('should redirect to /no-available path', () => {
+
+    const mockDomainService: unknown = {
+      isAvailable: () => of(false)
+    }
+
+    const guard = setup(mockDomainService);
+    guard.subscribe((p: unknown) => {
+      expect(p).toBe(false)
+      expect(mockRouter.navigate).toHaveBeenCalled()
+    })
+  })
 })
